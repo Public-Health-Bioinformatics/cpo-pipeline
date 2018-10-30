1200b2bd26acadb22d39f2b5173a1b1981e37e42
Dan Fornika <dfornika@gmail.com>
2018-10-29 20:25:13 -0700

    Test kraken parser by comparing to json file

--- assembly/test/test_parsers.py
+++ assembly/test/test_parsers.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+import json
 
 # add the '../../assembly' directory to the 'sys.path'
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -10,11 +11,13 @@ from parsers import result_parsers
 class KrakenResultParserTest(unittest.TestCase):
     def setUp(self):
         self.test_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/kraken_report.txt')
-        print(self.test_data_path)
+        with open('data/parsed_kraken_report.json') as kraken_report_json:
+            self.parsed_kraken_json = json.load(kraken_report_json)
+            kraken_report_json.close()
         
     def test_parse_kraken_result(self):
         parsed_result = result_parsers.parse_kraken_result(self.test_data_path)
-        self.assertTrue(False) # TBD
+        self.assertDictEqual(parsed_result, self.parsed_kraken_json)
     
 if __name__ == '__main__':
     unittest.main()
