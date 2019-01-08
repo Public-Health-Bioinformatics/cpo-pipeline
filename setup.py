8c44e63e8d305536b1f296588b347bbfa9582a9e
Dan Fornika <dfornika@gmail.com>
2019-01-08 13:43:55 -0800

    Restructure project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cpo_pipeline',
+    version='0.0.0',
+    description='An analysis pipeline for the purpose of investigating Carbapenemase-Producing Organisms.',
+    url='http://github.com/Public-Health-Bioinformatics/cpo-pipeline',
+    author='Dan Fornika, Justin Jia, Matthew Croxen, William Hsiao',
+    author_email='dan.fornika@bccdc.ca, bja20@sfu.ca, matthew.croxen@bccdc.ca, william.hsiao@bccdc.ca',
+    license='MIT',
+    packages=['cpo_pipeline'],
+    zip_safe=False
+)
