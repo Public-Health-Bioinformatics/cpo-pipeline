51b4f0e86f4cb590a837628517726a9c3fe20053
Dan Fornika <dfornika@gmail.com>
2019-01-09 17:28:21 -0800

    Setting up python project structure

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup
+from distutils.core import setup
+from setuptools import find_packages
+from cpo_pipeline import __version__
 
 setup(
     name='cpo_pipeline',
-    version='0.0.0',
+    version=__version__,
     description='An analysis pipeline for the purpose of investigating Carbapenemase-Producing Organisms.',
     url='http://github.com/Public-Health-Bioinformatics/cpo-pipeline',
     author='Dan Fornika, Justin Jia, Matthew Croxen, William Hsiao',
     author_email='dan.fornika@bccdc.ca, bja20@sfu.ca, matthew.croxen@bccdc.ca, william.hsiao@bccdc.ca',
     license='MIT',
     packages=['cpo_pipeline'],
-    zip_safe=False
+    include_package_data=True,
+    scripts=[
+        'bin/cpo-pipeline',
+    ],
+    zip_safe=False,
 )
